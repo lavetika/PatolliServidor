24049a4fb2391059c905e257f9c153373091eae3
jimenezd99 <diana.jimenezn99@gmail.com>
2020-11-15 21:38:24 -0700

    Creación de proyecto con jar Entidades

---
+++ src/patolliservidor/PrinciparServidor.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package patolliservidor;
+
+/**
+ *
+ * @author Diana Jiménez
+ */
+public class PrinciparServidor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
